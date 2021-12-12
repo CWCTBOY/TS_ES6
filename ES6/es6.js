e8f9bca8efc2f431db5dae71017d184663e6ad3a
CWCTBOY <qkrdlswo98@gmail.com>
2021-12-12 15:04:27 +0900

    this

--- ES6/es6.js
+++ ES6/es6.js
@@ -1,1 +1,29 @@
-console.log(this);
+// what "this" means?
+
+console.log(this); // 1. means "window" when excuting with console, window is 전역object
+
+const obj = {
+  name1: "Park",
+  age: 22,
+  함수1() {
+    console.log(this);
+  },
+  objInObj: {
+    name2: "Kim",
+    함수2() {
+      console.log(this);
+    }
+  }
+}
+
+obj.함수1();
+obj.objInObj.함수2(); // 2. means "obj" that contains function[console.log], the function in obj is called "method"
+
+function objMachine() {
+  this.name = "Park",
+    this.age = 22,
+    this.gender = "male"
+}
+
+const obj2 = new objMachine();
+console.log(obj2); // 3. 새로운 오브젝트 요소를 생성해냅니다. 오브젝트에 할당하는법은 위커맨드 참조
