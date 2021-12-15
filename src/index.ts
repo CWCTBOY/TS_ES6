6860446b7b2256a4d5a39276f5080be3c5a1b372
CWCTBOY <qkrdlswo98@gmail.com>
2021-12-15 21:39:04 +0900

    first TS

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello");
+type Project = {
+  member: string[];
+  days: number;
+  started: boolean;
+}
+
+let project: Project = {
+  member: ['kim', 'park'],
+  days: 30,
+  started: true,
+}
+
+const arr = [`Park`, 23, true, null];
+arr[4] = false;
+console.log(arr);
+
+console.log(33);
