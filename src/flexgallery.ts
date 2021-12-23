fa87dbf83594db0a3ba7fa77789e806c1e607620
CWCTBOY <qkrdlswo98@gmail.com>
2021-12-23 14:37:46 +0900

    type casting

--- src/flexgallery.ts
+++ src/flexgallery.ts
@@ -1,11 +1,15 @@
-const panel = document.querySelectorAll(`.panel`);
-const centerText = document.querySelectorAll(`.panel__centertext`);
+const panel = Array.from(document.querySelectorAll(`.panel`));
+const centerText = Array.from(document.querySelectorAll(`.panel__centertext`));
 // 장 펼치기, 흰색글씨 커지게하기
-panel.forEach(item => item.addEventListener(`mouseenter`, function (this: HTMLElement) {
-  this.classList.add(`open`);
-  item.children[1].classList.add(`font-size`);
-  this.addEventListener(`mouseleave`, function (this: HTMLElement) {
-    this.classList.remove(`open`);
-    item.children[1].classList.remove(`font-size`);
-  });
-})); 
+if (
+  panel instanceof HTMLDivElement &&
+  centerText instanceof HTMLSpanElement
+)
+  panel.forEach(item => item.addEventListener(`mouseenter`, function (this: HTMLElement) {
+    this.classList.add(`open`);
+    item.children[1].classList.add(`font-size`);
+    this.addEventListener(`mouseleave`, function (this: HTMLElement) {
+      this.classList.remove(`open`);
+      item.children[1].classList.remove(`font-size`);
+    });
+  })); 
